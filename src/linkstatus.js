8f19536c6364d89f44d1191e43596d5c69acb1d1
vyv03354 <VYV03354@nifty.ne.jp>
2023-06-25 22:38:05 +0900

    Supported dark mode.

--- src/linkstatus.js
+++ src/linkstatus.js
@@ -3,8 +3,8 @@ if (self == top) {
   iframe.frameBorder = 0;
   iframe.scrolling = 0;
   iframe.style.setProperty("background", "transparent", "important");
-  // Set `color-scheme` to the initial value so that the color matches with
-  // the iframe content. Othewise the iframe will lose transparency
+  // Set `color-scheme` to the initial value so that the color scheme matches
+  // with the iframe content. Othewise the iframe will lose transparency
   // (See https://bugzilla.mozilla.org/show_bug.cgi?id=1738380 for details).
   // We have to use `light` explicitly because `initial` does not seem to
   // initialize the `color-scheme` correctly if the page sets multiple
@@ -20,8 +20,12 @@ if (self == top) {
   iframe.style.margin = 0;
   iframe.style.padding = 0;
   iframe.style.zIndex = 2147483647;
+  iframe.onload = () => {
+    const darkModePref = matchMedia("(prefers-color-scheme: dark)");
+    iframe.contentDocument.styleSheets[1].disabled = !darkModePref.matches;
+  };
   document.documentElement.appendChild(iframe);
-  iframe.contentWindow.location = chrome.extension.getURL("status.html");
+  iframe.contentWindow.location = chrome.runtime.getURL("status.html");
 }
 let url = "";
 document.addEventListener("mouseover", function(e) {
